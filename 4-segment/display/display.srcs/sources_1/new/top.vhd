8453a69e0af27a989d020d60d5e35f95ec905add
Martin-Borka <65703172+Martin-Borka@users.noreply.github.com>
2023-02-28 13:24:28 +0100

    Update top.vhd

--- 4-segment/display/display.srcs/sources_1/new/top.vhd
+++ 4-segment/display/display.srcs/sources_1/new/top.vhd
@@ -81,19 +81,25 @@ begin
 
 -- Turn LED(4) on if input value is equal to 0, ie "0000"
 -- LED(4) <= `0` when WRITE YOUR CODE HERE
-    --if (blank = '1') then
-    --    LED(4) <= "1";
-
-LED(6) <= SW(0);
+    if (SW = '0000') then
+        LED(4) <= "1";
+   
 -- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
 -- LED(5) <= WRITE YOUR CODE HERE
+    
+   if (SW > '1000') then
+       LED(5) <= "1"; 
 
 -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
 -- LED(6) <= WRITE YOUR CODE HERE
+    
+LED(6) <= SW(0);
 
 -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
 -- LED(7) <= WRITE YOUR CODE HERE
 
+if (SW = '0001' OR SW = '0010' or SW = '0100' or SW = '1000') then
+    LED(7) <= "1";
 
 
 end architecture behavioral;
