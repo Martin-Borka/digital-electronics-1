997017c3285514a66d0f923ddee4038bc554b1aa
Martin-Borka <borka.martin01@gmail.com>
2023-04-19 23:12:05 +0200

    no TOP

--- ProjectUART/UARTv2/UARTv2.srcs/sources_1/new/top.vhd
+++ ProjectUART/UARTv2/UARTv2.srcs/sources_1/new/top.vhd
@@ -1,35 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/21/2023 11:51:40 AM
--- Design Name: 
--- Module Name: top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity top is
     Port ( CLK100MHZ : in STD_LOGIC;
@@ -41,64 +14,51 @@ entity top is
            CE : out STD_LOGIC;
            CF : out STD_LOGIC;
            CG : out STD_LOGIC;
-           DP : out STD_LOGIC;
            AN : out STD_LOGIC_VECTOR (7 downto 0);
-           BTNC : in STD_LOGIC);
+           BTNC : in STD_LOGIC;
+           LED : out STD_LOGIC_VECTOR (15 downto 0);
+           -- NEJAKY : out STD_LOGIC; --simulace
+           --nevim : out STD_LOGIC_VECTOR (7 downto 0);
+           JA : in STD_LOGIC_VECTOR (0 downto 0);
+           JB : out STD_LOGIC_VECTOR (0 downto 0)
+           );
 end top;
 
 architecture Behavioral of top is
+  signal sig_datap : std_logic_vector (7 downto 0);
 -- No internal signals are needed today:)
+
 begin
 
   --------------------------------------------------------
-  -- Instance (copy) of driver_7seg_4digits entity
+  -- Instance (copy) of driver_7seg_4digits 
   --------------------------------------------------------
-  driver_seg_4 : entity work.driver_7seg_8digits -- entity work.driver_7seg_8digits
-      port map (
-          clk      => CLK100MHZ,
-          rst      => BTNC,
-          
-                
-          data3(3) => SW(15),
-          data3(2) => SW(14),
-          data3(1) => SW(13),
-          data3(0) => SW(12),
-
-          -- MAP DATA INPUTS TO OTHER ON-BOARD SWITCHES HERE
-          data2(3)  =>  SW(11),
-          data2(2)  =>  SW(10),
-          data2(1)  =>  SW(9),
-          data2(0)  =>  SW(8),
-          
-          data1(3)  =>  SW(7),
-          data1(2)  =>  SW(6),
-          data1(1)  =>  SW(5),
-          data1(0)  =>  SW(4),
-          
-          
-          data0(3) => SW(3),
-          data0(2) => SW(2),
-          data0(1) => SW(1),
-          data0(0) => SW(0),
-
-          -- DECIMAL POINT
-          dp_vect => "01111111",
-          dp      => DP,
-
-          seg(6) => CA,
-          seg(5) => CB,
-          seg(4) => CC,
-          seg(3) => CD,
-          seg(2) => CE,
-          seg(1) => CF,
-          seg(0) => CG,
-          -- MAP OTHER DISPLAY SEGMENTS HERE
-
-
-          -- DIGITS
-          dig(7 downto 0) => AN(7 downto 0)
-      );
-
+ 
+ vysilac : entity work.vysilac
+    port map(
+    
+        --napsat e SW= LED
+        prepinac => SW(15),
+        clk => CLK100MHZ,
+        rst => BTNC,
+        -- vystup => NEJAKY, -- simulace
+        data0 => SW(0),
+        data1 => SW(1),
+        data2 => SW(2),
+        data3 => SW(3),
+        data4 => SW(4),
+        data5 => SW(5),
+        data6 => SW(6),
+        data7 => SW(7),
+        
+        SW(2) => SW(13),
+        SW(1) => SW(12),
+        SW(0) => SW(11),
+        vstup => JA(0),
+        vystup => JB(0), -- implementace
+        vysledek => sig_datap
+        );
+        
   --------------------------------------------------------
   -- Other settings
   --------------------------------------------------------
